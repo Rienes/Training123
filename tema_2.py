1045cb698ce214fd5ac75ce28e5ed72b60928bf1
Алексей2 <2>
2020-09-27 16:07:54 +0300

    start

---
+++ tema_2.py
@@ -0,0 +1,126 @@
+#  Задачи темы № 2
+# ____________________________________________________
+
+# __________________________A
+
+
+# a = int(input('a'))
+# b = int(input('b'))
+# print((a**2+b**2)**0.5)
+
+# __________________________B
+
+# print(f'Hello, {input("Name ")}!')
+
+# __________________________C
+
+# number = int(input())
+# print(
+#     f'The next number for the number {number} is {number + 1}.\n'
+#     f'The previous number for the number {number} is {number - 1}.')
+
+# __________________________D
+
+# print(int(input('apple: ')) // int(input('People: ')))
+
+# __________________________E
+
+# print(int(input('apple: ')) % int(input('People: ')))
+
+# __________________________F
+
+# print(int(input('V = ')) * int(input('t = ')) % 109)
+
+# __________________________G
+
+# print(int(input('Number = ')) % 10)
+
+# __________________________H
+
+# print(int(input('Number = ')) // 10)
+
+# __________________________I
+
+# print(int(input('Number = ')) // 10 % 10)
+
+# __________________________J
+
+# number = int(input('Num = '))
+# last = number % 10
+# middle = (number // 10) % 10
+# first = number // 100
+# summ = last+middle+first
+# print(summ)
+
+# __________________________K
+
+# number = int(input('Num = '))
+# print(number + 1 + (number + 1) % 2)
+
+# __________________________L
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# l = int(input('l = '))
+# n = int(input('n = '))
+#
+# lenght = (n-1)*(2*a+2*b)+2*l+a
+# print(lenght)
+
+# __________________________M
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# l = int(input('l = '))
+#
+# print((a + b + l) // 2 + (a + b + l) % 2)
+
+# __________________________N
+
+# m = int(input('m = '))
+# print((m // 60) % 24, m % 60)
+
+# __________________________O
+
+# s = int(input('s = '))
+# s = 3602
+# seconds = s % 60
+# minutes = s % 3600 // 60
+# hours = s // 3600 % 24
+#
+# print(f'{hours:0>2}:{minutes:0>2}:{seconds:0>2}')
+
+
+# s = 129700
+# seconds = s % 60 // 10
+# seconds_2 = s % 10
+#
+# minutes = s // 60 % 60 // 10
+# minutes_2 = s // 60 % 60 % 10
+#
+# hours = s // 3600 % 24
+#
+# print(f'{hours}:{minutes}{minutes_2}:{seconds}{seconds_2}')
+
+# __________________________P
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print(a, b)
+#
+# c = a
+# a = b
+# b = c
+#
+# print(a, b)
+
+
+# __________________________Q
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+
+# __________________________R
